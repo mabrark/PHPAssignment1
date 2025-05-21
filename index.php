e21b6cd1b1e8744d5226e7029c3882d2621035f4
mabrark <muhammad.khan@triosstudent.com>
2025-05-21 19:23:31 -0400

    added header and footer

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <!DOCTYPE html>
 <html>
-    <head>
-    </head>
-    <body>
+     <head>
+       <title>Library - Home</title>
+       <link rel="stylesheet" type="txt/css" herf="css/main.css" />
+     </head>
+     <body>
+        <?php include("header.php"); ?>
+
+        <main>
+          <h2>Book List</h2>
+        </main> 
+        
+        <?php include("footer.php"); ?>   
   
-    </body>
+     </body>
 </html>
