053b753d6f1304b94adb0e53978e7ddfd62b7465
mabrark <muhammad.khan@triosstudent.com>
2025-05-21 21:42:15 -0400

    added database files

--- index.php
+++ index.php
@@ -9,8 +9,17 @@
 
         <main>
           <h2>Book List</h2>
+
+          <table>
+            <tr>
+              <th>Book Name</th>
+              <th>Author Name</th>
+              <th> Publisher</th>
+              <th>Year</th>
+              </tr>
+          </table>
         </main> 
-        
+
         <?php include("footer.php"); ?>   
   
      </body>
