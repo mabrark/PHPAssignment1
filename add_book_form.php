bc36a4b021c7ed30571d1c60c5b0b4b9d8c39f10
mabrark <muhammad.khan@triosstudent.com>
2025-05-28 18:06:19 -0400

    fixed code

--- add_book_form.php
+++ add_book_form.php
@@ -3,7 +3,7 @@
 <html>
      <head>
        <title>Library Stock - add Book</title>
-       <link rel="stylesheet" type="txt/css" href="css/main.css" />
+       <link rel="stylesheet" type="text/css" href="css/main.css" />
      </head>
      <body>
         <?php include("header.php"); ?>
