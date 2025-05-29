7bc5ee0cf14d05a6de7340c239aecc489c110d15
mabrark <muhammad.khan@triosstudent.com>
2025-05-29 19:33:51 -0400

    added fixed codes

--- add_book.php
+++ add_book.php
@@ -1,59 +1,62 @@
 
 <?php
    session_start();
-
-   require_once 'image_util.php'; 
-
+   echo "Test0";
+ 
+   require_once 'image_util.php';
+ 
     $image_dir = 'images';
     $image_dir_path = getcwd() . DIRECTORY_SEPARATOR . $image_dir;
-
+ 
     if (isset($_FILES['file1']))
     {
         $filename = $_FILES['file1']['name'];
-
+ 
         if (!empty($filename))
         {
+            echo "Test1";
             $source = $_FILES['file1']['tmp_name'];
             $target = $image_dir_path . DIRECTORY_SEPARATOR . $filename;
-
+ 
             move_uploaded_file($source, $target);
-
+            echo "Test2";
+ 
             process_image($image_dir_path, $filename);
         }
     }
-
+ 
    $book_name = filter_input(INPUT_POST, 'book_name');
    $author_name = filter_input(INPUT_POST, 'author_name');
    $publisher = filter_input(INPUT_POST, 'publisher');
    $status = filter_input(INPUT_POST, 'status'); // assigns the value of the selected radio button
    $year = filter_input(INPUT_POST, 'year');
    $image_name = $_FILES['file1']['name'];
-
+ 
     require_once('database.php');
     $queryBooks = 'SELECT * FROM books';
     $statement1 = $db->prepare($queryBooks);
     $statement1->execute();
     $books = $statement1->fetchAll();
-
+ 
     $statement1->closeCursor();
-
+ 
     foreach ($books as $book)
-    { 
+    {
       if ($book_name == $book["bookName"])
       {
-        $_SESSION["add_error"] = "invalid data, duplicate book name. Try again."
-
+        $_SESSION["add_error"] = "invalid data, duplicate book name. Try again.";
+ 
         $url = "error.php";
         header("Location: " . $url);
         die();
       }
     }
-
+ 
     if ($book_name == null || $author_name == null ||
         $publisher == null || $year == null)
         {
-          $_SESSION["add_error"] = "invalid book data, check all fields and Try again."
-
+          $_SESSION["add_error"] = "invalid book data, check all fields and Try again.";
+ 
           $url = "error.php";
           header("Location: " . $url);
           die();
@@ -61,12 +64,12 @@
         else
         {
           require_once('database.php');
-
-       $query = 'INSERT INTO BOOKS
+ 
+       $query = 'INSERT INTO books
           (bookName, authorName, publisher, status, year, imageName)
           VALUES
           (:bookName, :authorName, :publisher, :status, :year, :imageName)';
-
+ 
        $statement = $db->prepare($query);
        $statement->bindValue(':bookName', $book_name);
        $statement->bindValue(':authorName', $author_name);
@@ -75,15 +78,15 @@
        $statement->bindValue(':year', $year);
        $statement->bindValue(':imageName', $image_name);
    
-       $statement->executed();
-       $statement->loseCurser();
-
- }
-
+       $statement->execute();
+       $statement->closeCursor();
+ 
+}
+ 
    $_SESSION["fullName"] = $book_name . " " . $author_name;
-
+ 
    $url = "confirmation.php";
    header("Location: " . $url);
-   die(); 
-
+   die();
+ 
 ?>
