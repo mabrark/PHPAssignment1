d3eb091ca6aadb6aadf5607760acf72c41935e43
mabrark <muhammad.khan@triosstudent.com>
2025-06-09 12:01:00 -0400

    added placeholder

--- add_book.php
+++ add_book.php
@@ -1,51 +1,27 @@
 
 <?php
    session_start();
- 
-   require_once 'image_util.php';
- 
-    $image_dir = 'images';
-    $image_dir_path = getcwd() . DIRECTORY_SEPARATOR . $image_dir;
- 
-    if (isset($_FILES['file1']))
-    {
-        $filename = $_FILES['file1']['name'];
- 
-        if (!empty($filename))
-        {
-            $source = $_FILES['file1']['tmp_name'];
-            $target = $image_dir_path . DIRECTORY_SEPARATOR . $filename;
- 
-            move_uploaded_file($source, $target);
- 
-            process_image($image_dir_path, $filename);
-        }
-    }
- 
+   echo "Test0";
    $book_name = filter_input(INPUT_POST, 'book_name');
    $author_name = filter_input(INPUT_POST, 'author_name');
    $publisher = filter_input(INPUT_POST, 'publisher');
-   $status = filter_input(INPUT_POST, 'status'); // assigns the value of the selected radio button
+   $status = filter_input(INPUT_POST, 'status');
    $year = filter_input(INPUT_POST, 'year');
    $type_id = filter_input(INPUT_POST, 'type_id', FILTER_VALIDATE_INT);
-
-   $file_name = $_FILES['file1']['name'];
-
-   $i = strrpos($filename, '.');
-   $image_name = substr($filename, 0, $i);
-   $ext = substr($filename, $i);
-
-   $image_name_100 = $image_name . '_100' . $ext;
+   $image = $_FILES['image'];
  
-    require_once('database.php');
+   require_once('database.p[hp')
+   require_once('image_util.php');
+
+   $base_dir = 'image/';
+
+   require_once('database.php');
     $queryBooks = 'SELECT * FROM books';
     $statement1 = $db->prepare($queryBooks);
     $statement1->execute();
     $books = $statement1->fetchAll(); 
     $statement1->closeCursor();
 
-    
- 
     foreach ($books as $book)
     {
       if ($book_name == $book["bookName"])
@@ -57,19 +33,36 @@
     }
  
     if ($book_name == null || $author_name == null ||
-        $publisher == null || $status == null $year == null $type == null);
-        {
+        $publisher == null || $status == null $year == null $type_id == null) {
           $_SESSION["add_error"] = "Invalid book data, check all fields and Try again.";
           header("Location:  . error.php");
           die();
         }
-        else
-        {
- 
+        $image_name = '';
+
+        if ($image && $image['error'] === UPLOAD_ERR_OK) {
+          $original_filrname = basename($image['name']);
+          $upload_path = $base_dir . $original_filrname;
+          move_uploaded_file($image['tmp_name'], $upload_path);
+          process_image($base_dir, $original_filrname);
+
+          $dot_pos = strrpos($original_filrname, '.');
+          $name_100 = substr($original_filrname, 0, $dot_pos) . '_100' . substr($original_filrname, $dot_pos);
+          $image_name = $name_100;
+        } else {
+          $placeholder_100 = 'placeholder_100.jpg';
+          $placeholder_400 = 'placeholder_400.jpg';
+
+          if (!file_exists($base_dir . $placeholder_100) || !file_exists($base_dir . $placeholder_400)) {
+            process_image($base_dir, $placeholder);
+          }
+          $image_name = $placeholder_100;
+        }
+
           $query = 'INSERT INTO books
-          (bookName, authorName, publisher, status, year, imageName, typeID)
+          (bookName, authorName, publisher, status, year, typeID, imageName)
           VALUES
-          (:bookName, :authorName, :publisher, :status, :year, :imageName, :typeID)';
+          (:bookName, :authorName, :publisher, :status, :year, :typeID, :imageName)';
  
        $statement = $db->prepare($query);
        $statement->bindValue(':bookName', $book_name);
@@ -77,12 +70,10 @@
        $statement->bindValue(':publisher', $publisher);
        $statement->bindValue(':status', $status);   
        $statement->bindValue(':year', $year);
-       $statement->bindValue(':imageName', $image_name_100);
-       $statement->bindValue(':typeID' $type_id);  
+       $statement->bindValue(':typeID' $type_id); 
+       $statement->bindValue(':imageName', $image_name_100);    
        $statement->execute();
        $statement->closeCursor();
- 
-}
  
    $_SESSION["fullName"] = $book_name . " " . $author_name;
  
